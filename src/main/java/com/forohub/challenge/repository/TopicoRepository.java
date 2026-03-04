26340b506752ccaac1b4e759c503e157b3ff1f48
Encarnación5012 <estainlinv@gmail.com>
2026-03-04 11:54:10 -0400

    Refactorización a arquitectura de capas y configuración de seguridad

--- src/main/java/com/forohub/challenge/repository/TopicoRepository.java
+++ src/main/java/com/forohub/challenge/repository/TopicoRepository.java
@@ -1,12 +1,11 @@
 package com.forohub.challenge.repository;
 
 import com.forohub.challenge.model.Topico;
-import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
